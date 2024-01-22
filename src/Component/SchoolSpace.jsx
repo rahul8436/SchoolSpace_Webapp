8b6e5635baff89d657fde3ef2a7019a2affe2f5d
rahul8436 <rahulkundu2016@gmail.com>
2024-01-22 20:16:00 +0530

    added svgs

--- src/Component/SchoolSpace.jsx
+++ src/Component/SchoolSpace.jsx
@@ -1,25 +1,43 @@
 import React from "react";
 import {
-  Table,
-  TableHead,
-  TableCell,
-  Paper,
-  TableRow,
-  TableBody,
-  Button,
-  styled,
-  Box,
+  Box
 } from "@mui/material";
+import { ReactComponent as NotificationSvg } from "../Assets/svgs/Notification.svg";
+import { ReactComponent as NoticeBoardSvg } from "../Assets/svgs/Notice_Board.svg";
+import { ReactComponent as DashboardSvg } from "../Assets/svgs/Dashboard.svg";
+import { ReactComponent as LiveClassesSvg } from "../Assets/svgs/Live_Classes.svg";
+import { ReactComponent as ExamsSvg } from "../Assets/svgs/Exams.svg";
+import { ReactComponent as StudentsSvg } from "../Assets/svgs/Students.svg";
+import { ReactComponent as ResultsSvg } from "../Assets/svgs/Results.svg";
+import { ReactComponent as CoursesSvg } from "../Assets/svgs/Courses.svg";
+
+const commonMenuItemStyle = {
+  borderRadius: "10px",
+  margin: "auto",
+  display: "flex",
+  gap: "18px",
+  width: "240px",
+  height: "40px",
+  marginTop: "10px",
+};
+
+const commonTextStyle = {
+  fontFamily: "Montserrat",
+  fontWeight: 400,
+  fontSize: "14px",
+  lineHeight: "18px",
+  color: "#242424",
+};
 
 const SchoolSpace = () => {
   return (
     <>
       <Box
         sx={{
-          width: "20%",
-          margin: "15px",
-          // border: "2px solid green",
-          height: "auto",
+          width: "288px",
+          height: "1024px",
+          background: "#FFF",
+          boxShadow: "0px 3px 14px 2px rgba(0, 0, 0, 0.05), 0px 8px 10px 1px rgba(0, 0, 0, 0.05), 0px 5px 5px -3px rgba(0, 0, 0, 0.05)",
         }}
       >
         <div
@@ -31,18 +49,16 @@ const SchoolSpace = () => {
           }}
         >
           <div
-            //    sx={style}
             style={{
               width: "60px",
               height: "40px",
-              // border: "2px solid red",
-
               width: "60px",
               height: "48px",
               left: "30px",
               top: "40px",
               borderRadius: "8px",
               backgroundColor: "#FFBF3F",
+              marginTop: "40px",
             }}
           >
             <img
@@ -61,294 +77,74 @@ const SchoolSpace = () => {
             <div
               style={{
                 position: "absolute",
-
+                marginTop: "44px",
                 marginLeft: "10px",
-                lineHeight: "50px",
+                lineHeight: "40px",
                 fontFamily: "Passion One",
                 fontStyle: "normal",
                 fontWeight: "400",
                 fontSize: "32px",
-
-                /* identical to box height, or 125% */
-
                 color: "#2CA4D8",
               }}
             >
-              SchoolSpace
+              School Space
             </div>
           </div>
-        </div>
-        <br />
+        </div>        <br />
         <hr style={{ width: "80%" }} />
         <div
           style={{
-            // border: "2px solid yellow",
             height: "auto",
             width: "100%",
             margin: "auto",
           }}
         >
-          <div
-            style={{
-              borderRadius: "10px",
-              margin: "auto",
-              display: "flex",
-              gap: "18px",
-              width: "240px",
-              height: "40px",
-              marginTop: "10px",
-              // border: "2px solid green",
-            }}
-          >
-            <img
-              src="../Assets/Images/Dashboard.png"
-              alt=""
-              srcset=""
-              style={{
-                width: "18px",
-                height: "18px",
-                margin: "14px 0 0 3px",
-              }}
-            />
-            <p
-              style={{
-                fontFamily: "Montserrat",
-                weight: 600,
-                lineHeight: "14px",
-              }}
-            >
-              DashBoard
-            </p>
+          {/* Dashboard */}
+          <div style={commonMenuItemStyle}>
+            <DashboardSvg style={{ width: "24px", height: "24px", margin: "14px 0 0 3px" }} />
+            <p style={commonTextStyle}>Dashboard</p>
           </div>
 
-          <div
-            style={{
-              borderRadius: "10px",
-              margin: "auto",
-              display: "flex",
-              gap: "18px",
-              width: "240px",
-              height: "40px",
-              marginTop: "10px",
-              // border: "2px solid green",
-            }}
-          >
-            <img
-              src="../Assets/Images/Courses.png"
-              alt=""
-              srcset=""
-              style={{
-                width: "18px",
-                height: "18px",
-                margin: "14px 0 0 3px",
-              }}
-            />
-            <p
-              style={{
-                fontFamily: "Montserrat",
-                weight: 600,
-                lineHeight: "14px",
-              }}
-            >
-              Courses
-            </p>
+          {/* Courses */}
+          <div style={commonMenuItemStyle}>
+            <CoursesSvg style={{ width: "24px", height: "24px", margin: "14px 0 0 3px" }} />
+            <p style={commonTextStyle}>Courses</p>
           </div>
 
-          <div
-            style={{
-              borderRadius: "10px",
-              margin: "auto",
-              display: "flex",
-              gap: "18px",
-              width: "240px",
-              height: "40px",
-              marginTop: "10px",
-              border: "2px solid #2CA4D8",
-            }}
-          >
-            <img
-              src="../Assets/Images/Students.png"
-              alt=""
-              srcset=""
-              style={{
-                width: "18px",
-                height: "18px",
-                margin: "14px 0 0 3px",
-              }}
-            />
-            <p
-              style={{
-                fontFamily: "Montserrat",
-                weight: 600,
-                lineHeight: "14px",
-              }}
-            >
-              Students
-            </p>
+          {/* Students */}
+          <div style={{ ...commonMenuItemStyle, border: "2px solid #2CA4D8" }}>
+            <StudentsSvg style={{ width: "24px", height: "24px", margin: "14px 0 0 3px" }} />
+            <p style={{ ...commonTextStyle, color: "#2CA4D8", fontWeight: 600 }}>Students</p>
           </div>
 
-          <div
-            style={{
-              borderRadius: "10px",
-              margin: "auto",
-              display: "flex",
-              gap: "18px",
-              width: "240px",
-              height: "40px",
-              marginTop: "10px",
-              // border: "2px solid green",
-            }}
-          >
-            <img
-              src="../Assets/Images/Exams.png"
-              alt=""
-              srcset=""
-              style={{
-                width: "18px",
-                height: "18px",
-                margin: "14px 0 0 3px",
-              }}
-            />
-            <p
-              style={{
-                fontFamily: "Montserrat",
-                weight: 600,
-                lineHeight: "14px",
-              }}
-            >
-              Exams
-            </p>
-          </div>
-          <div
-            style={{
-              borderRadius: "10px",
-              margin: "auto",
-              display: "flex",
-              gap: "18px",
-              width: "240px",
-              height: "40px",
-              marginTop: "10px",
-              // border: "2px solid green",
-            }}
-          >
-            <img
-              src="../Assets/Images/Result.png"
-              alt=""
-              srcset=""
-              style={{
-                width: "18px",
-                height: "18px",
-                margin: "14px 0 0 3px",
-              }}
-            />
-            <p
-              style={{
-                fontFamily: "Montserrat",
-                weight: 600,
-                lineHeight: "14px",
-              }}
-            >
-              Results
-            </p>
+          {/* Exams */}
+          <div style={commonMenuItemStyle}>
+            <ExamsSvg style={{ width: "24px", height: "24px", margin: "14px 0 0 3px" }} />
+            <p style={commonTextStyle}>Exams</p>
           </div>
 
-          <div
-            style={{
-              borderRadius: "10px",
-              margin: "auto",
-              display: "flex",
-              gap: "18px",
-              width: "240px",
-              height: "40px",
-              marginTop: "10px",
-              // border: "2px solid green",
-            }}
-          >
-            <img
-              src="../Assets/Images/Noticeboard.png"
-              alt=""
-              srcset=""
-              style={{
-                width: "18px",
-                height: "18px",
-                margin: "14px 0 0 3px",
-              }}
-            />
-            <p
-              style={{
-                fontFamily: "Montserrat",
-                weight: 600,
-                lineHeight: "14px",
-              }}
-            >
-              Notice Board
-            </p>
+          {/* Results */}
+          <div style={commonMenuItemStyle}>
+            <ResultsSvg style={{ width: "24px", height: "24px", margin: "14px 0 0 3px" }} />
+            <p style={commonTextStyle}>Results</p>
           </div>
 
-          <div
-            style={{
-              borderRadius: "10px",
-              margin: "auto",
-              display: "flex",
-              gap: "18px",
-              width: "240px",
-              height: "40px",
-              marginTop: "10px",
-              // border: "2px solid green",
-            }}
-          >
-            <img
-              src="../Assets/Images/LiveClassess.png"
-              alt=""
-              srcset=""
-              style={{
-                width: "18px",
-                height: "18px",
-                margin: "14px 0 0 3px",
-              }}
-            />
-            <p
-              style={{
-                fontFamily: "Montserrat",
-                weight: 600,
-                lineHeight: "14px",
-              }}
-            >
-              Live Classes
-            </p>
+          {/* Notice Board */}
+          <div style={commonMenuItemStyle}>
+            <NoticeBoardSvg style={{ width: "24px", height: "24px", margin: "14px 0 0 3px" }} />
+            <p style={commonTextStyle}>Notice Board</p>
           </div>
 
-          <div
-            style={{
-              borderRadius: "10px",
-              margin: "auto",
-              display: "flex",
-              gap: "18px",
-              width: "240px",
-              height: "40px",
-              marginTop: "10px",
-              // border: "2px solid green",
-            }}
-          >
-            <img
-              src="../Assets/Images/Notification.png"
-              alt=""
-              srcset=""
-              style={{
-                width: "18px",
-                height: "18px",
-                margin: "14px 0 0 3px",
-              }}
-            />
-            <p
-              style={{
-                fontFamily: "Montserrat",
-                weight: 600,
-                lineHeight: "14px",
-              }}
-            >
-              Notifications
-            </p>
+          {/* Live Classes */}
+          <div style={commonMenuItemStyle}>
+            <LiveClassesSvg style={{ width: "24px", height: "24px", margin: "14px 0 0 3px" }} />
+            <p style={commonTextStyle}>Live Classes</p>
+          </div>
+
+          {/* Notifications */}
+          <div style={commonMenuItemStyle}>
+            <NotificationSvg style={{ width: "24px", height: "24px", margin: "14px 0 0 3px" }} />
+            <p style={commonTextStyle}>Notifications</p>
           </div>
         </div>
       </Box>
